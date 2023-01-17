5b784b3a46bc2ca7a614847943b34bb9ecd7aad1
Lidor York <yorklidor@gmail.com>
2023-01-17 20:53:37 +0200

    adding basic sass folders

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import { store } from './store/store'
 import { RootCmp } from './root-cmp'
+import './assets/styles/main.scss'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
