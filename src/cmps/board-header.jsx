27fd1fe5aa4a6bc2c16737f7b698198fb0ff7a6c
matanad <matan19949@gmail.com>
2023-01-17 23:26:24 +0200

    more styling

--- src/cmps/board-header.jsx
+++ src/cmps/board-header.jsx
@@ -4,15 +4,18 @@ export function BoardHeader() {
         <div
             contentEditable={true}
             suppressContentEditableWarning={true}
+            multiline={false}
         >
             <h1 className="title">Sprint 4</h1>
         </div>
         <div className="actions-container">
             <button className="btn btn-filter">Filter</button>
+            <span className="btn-divider"></span>
             <button className="btn btn-share">Share</button>
+            <span className="btn-divider"></span>
             <button className="btn btn-menu">
                 <span>...</span>
-                </button>
+            </button>
         </div>
     </section>
 }
