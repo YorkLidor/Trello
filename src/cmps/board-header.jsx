a93fff7427bd02af29dd2c15798cea62da489892
matanad <matan19949@gmail.com>
2023-01-18 00:33:23 +0200

    padding fix

--- src/cmps/board-header.jsx
+++ src/cmps/board-header.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useRef, useState } from "react"
+
 export function BoardHeader() {
+    const [editClass, setEditClass] = useState('')
+    const elTitleContainer = useRef(null)
+
+    function onTitleEditToggle(classToSet) {
+        setEditClass(classToSet)
+        elTitleContainer.current.className.add('editable')
+    }
 
     return <section className="board-header flex justify-between">
         <div
-            contentEditable={true}
-            suppressContentEditableWarning={true}
-            multiline={false}
+            className={`title-container ${editClass}`}
+            ref={elTitleContainer}
         >
-            <h1 className="title">Sprint 4</h1>
+            <h1
+                className="board-title-header"
+                onClick={() => onTitleEditToggle('editable')}
+            >
+                Sprint 4
+            </h1>
+            <input
+                className="board-title-input"
+                type="text"
+                value="Sprint 4"
+                name="title"
+                onChange={console.log}
+                onBlur={() => onTitleEditToggle('')}
+            />
         </div>
         <div className="actions-container">
             <button className="btn btn-filter">Filter</button>
