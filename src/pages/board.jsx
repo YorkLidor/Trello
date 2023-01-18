ff75928f412e9600d83a6cebe22522fb250eb82b
matanad <matan19949@gmail.com>
2023-01-18 21:49:02 +0200

    added the option to create a new board

--- src/pages/board.jsx
+++ src/pages/board.jsx
@@ -18,8 +18,8 @@ export function Board() {
     async function loadBoard() {
         try {
             const board = await boardService.getById(boardId)
-            console.log('boardId', boardId, boardId);
             setBoard(board)
+            document.body.style.backgroundImage  = `url(${board.style.bg})`
         } catch (err) {
             console.error('No Board!', err)
         }
