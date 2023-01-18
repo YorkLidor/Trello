500a27735629dcb3865a9f126836539eb063dba2
Lidor York <yorklidor@gmail.com>
2023-01-18 12:51:33 +0200

    fixing board layout

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Routes, Route } from 'react-router'
 
 import { AppHeader } from './cmps/app-header'
-import { AppFooter } from './cmps/app-footer'
 import { Board } from './pages/board'
 import { HomePage } from './pages/home-page'
 import { WorkSpace } from './pages/work-space'
@@ -11,18 +10,16 @@ import { TaskDetails } from './pages/task-details'
 export function RootCmp() {
 
     return (
-        <div>
+        <>
             <AppHeader />
-            <main>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/workspace" element={<WorkSpace />} />
-                    <Route path="/board" element={<Board />} />
-                    <Route path="/c/:groupId/:taskId" element={<TaskDetails />} />
-                </Routes>
-            </main>
-            <AppFooter />
-        </div>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/workspace" element={<WorkSpace />} />
+                <Route path="/board" element={<Board />} />
+                <Route path="/c/:groupId/:taskId" element={<TaskDetails />} />
+            </Routes>
+
+        </>
     )
 }
 
