aa8ce427cd68a510f1983d781d56218d6bccb3fa
matanad <matan19949@gmail.com>
2023-01-20 00:14:57 +0200

    tasks now dragable!!

--- src/cmps/task-list.jsx
+++ src/cmps/task-list.jsx
@@ -1,10 +1,51 @@
+import { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { utilService } from "../services/util.service";
 import { TaskPreview } from "./task-preview";
 
-export function TaskList({ group , boardId}) {
+export function TaskList({ group, boardId }) {
+    const [tasks, setTasks] = useState(group.tasks)
 
-    return <ul className="task-list">
-        {
-            group.tasks.map((task) => <TaskPreview boardId={boardId} task={task} key={task.id} group={group} />)
-        }
-    </ul>
+    function onEnd(res) {
+        const { source, destination } = res
+        setTasks(utilService.reorder(tasks, source.index, destination.index))
+
+    }
+
+    return <DragDropContext onDragEnd={onEnd}>
+        <Droppable droppableId="group-container">
+            {provided =>
+                <ul
+                    className="task-list"
+                    ref={provided.innerRef}
+                >
+                    {tasks.map((task, idx) =>
+                        <Draggable
+                            draggableId={task.id}
+                            key={task.id}
+                            index={idx}
+                        >
+                            {provided =>
+                                <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                >
+
+                                    {console.log('task:', task, idx)}
+                                    <TaskPreview
+                                        boardId={boardId}
+                                        task={task}
+                                        group={group}
+
+                                    />
+                                </div>
+                            }
+                        </Draggable>
+                    )}
+                    {provided.placeholder}
+                </ul>
+            }
+        </Droppable >
+    </DragDropContext>
 }
