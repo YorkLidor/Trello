daee00ab31b9cf46db5f54e63e1c5ac2dfa07bda
matanad <matan19949@gmail.com>
2023-01-19 13:01:58 +0200

    redux added

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -5,21 +5,23 @@ import { AppHeader } from './cmps/app-header'
 import { Board } from './pages/board'
 import { HomePage } from './pages/home-page'
 import { TaskDetails } from './pages/task-details'
-import { BoardsIndex } from './pages/boards-index'
+import { BoardIndex } from './pages/board-index'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 export function RootCmp() {
 
     return (
-        <>
+        <Provider store={store}>
             <AppHeader />
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/workspace" element={<BoardsIndex />} />
+                <Route path="/workspace" element={<BoardIndex />} />
                 <Route path="/board/:boardId" element={<Board />} />
                 <Route path="/card/:boardId/:groupId/:taskId" element={<TaskDetails />} />
             </Routes>
 
-        </>
+        </Provider>
     )
 }
 
