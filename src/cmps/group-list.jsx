318b4e9c59ecbdbeb3990463454bd8d363f7d3ce
matanad <matan19949@gmail.com>
2023-01-21 01:36:06 +0200

    dnd fully functional! 🎉🎉🎉

--- src/cmps/group-list.jsx
+++ src/cmps/group-list.jsx
@@ -8,11 +8,8 @@ import { GroupAdd } from "./group-add";
 import { utilService } from "../services/util.service";
 
 export function GroupList({ groups, board }) {
-    const [stateGroups, setGroups] = useState(groups)
     const [groupToEdit, setGroupToEdit] = useState(boardService.getEmptyGroup())
     const setBoard = []
-
-    console.log('render')
 
     async function onAddGroup(ev) {
         ev.preventDefault()
@@ -29,43 +26,63 @@ export function GroupList({ groups, board }) {
         setGroupToEdit({ ...groupToEdit, title: value })
     }
 
-    function onEnd({ source, destination }) {
-        console.log('source:', source)
-        console.log('destination:', destination)
-        const groupToEdit = board.groups.find((group, idx) => group.id === destination.droppableId)
-        const groupFrom = board.groups.find((group)=> group.id === source.droppableId)
-        const tasks = groupToEdit.tasks
+    function onDragEnd({ source, destination, type }) {
+        if (type === 'task-list') {
+            const groupToEdit = board.groups.find(group => group.id === destination.droppableId)
+            const groupFrom = board.groups.find(group => group.id === source.droppableId)
+            const tasks = groupToEdit.tasks
 
-        if (source.droppableId === destination.droppableId) {
-            groupToEdit.tasks = utilService.reorder(tasks, source.index, destination.index)
-            setGroups([...board.groups])
-            setset({ ...board })
-            saveBoard({ ...board })
+            if (source.droppableId === destination.droppableId) {
+                groupToEdit.tasks = utilService.reorder(tasks, source.index, destination.index)
+                setset({ ...board })
+                saveBoard({ ...board })
+            } else {
+                const deletedTask = groupFrom.tasks.splice(source.index, 1)
+                groupToEdit.tasks.push(...deletedTask)
+                groupToEdit.tasks = utilService.reorder(tasks, tasks.length - 1, destination.index)
+                setset({ ...board })
+                saveBoard({ ...board })
+            }
         } else {
-            const deletedTask = groupFrom.tasks.splice(source.index, 1)
-            groupToEdit.tasks.push(...deletedTask)
-            groupToEdit.tasks = utilService.reorder(tasks, tasks.length -1, destination.index)
-            console.log('deletedTask:', deletedTask)
-            console.log('groupToEdit.tasks:', groupToEdit.tasks)
+            board.groups = utilService.reorder(board.groups, source.index, destination.index)
             setset({ ...board })
             saveBoard({ ...board })
         }
-
     }
 
-    return <DragDropContext onDragEnd={onEnd}>
-        <ul className="group-list-container">
-            {
-                stateGroups.map((group) =>
-                    <Group
-                        key={group.id}
-                        group={group}
-                        setBoard={setBoard}
-                        board={board}
-                    />)
-            }
+    return <DragDropContext onDragEnd={onDragEnd} >
+        <Droppable droppableId={board._id} direction='horizontal' type="grooup-list">
+            {provided =>
+                <ul className="group-list-container" ref={provided.innerRef}>
+                    {
+                        groups.map((group, idx) =>
+                            <Draggable
+                                draggableId={group.id}
+                                key={group.id}
+                                index={idx}
+                            >
+                                {provided =>
+                                    <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                    >
 
-            <GroupAdd onAddGroup={onAddGroup} handleChange={handleChange} />
-        </ul>
+                                        <Group
+                                            provided={provided}
+                                            key={group.id}
+                                            group={group}
+                                            setBoard={setBoard}
+                                            board={board}
+                                            idx={idx}
+                                        />
+                                    </div>
+                                }
+                            </Draggable >
+                        )}
+                    {provided.placeholder}
+                    <GroupAdd onAddGroup={onAddGroup} handleChange={handleChange} />
+                </ul>
+            }
+        </Droppable >
     </DragDropContext>
 }
--- src/cmps/group.jsx
+++ src/cmps/group.jsx
@@ -1,18 +1,20 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd"
 import { GroupFooter } from "./group-footer"
 import { GroupHeader } from "./group-header"
 import { TaskList } from "./task-list"
 
-export function Group({ group, setBoard, board }) {
+export function Group({ group, setBoard, board, provided }) {
 
 
     return <li className="group-item-container">
-        <div className="group-item">
-
-            <GroupHeader group={group} setBoard={setBoard} board={board} />
-
-            <TaskList boardId={board._id} group={group} />
-
-            <GroupFooter boardId={board._id} group={group} setBoard={setBoard}/>
-        </div>
+        
+                    <div className="group-item">
+                        <div {...provided.dragHandleProps}>
+                            <GroupHeader group={group} setBoard={setBoard} board={board} />
+                        </div>
+                        <TaskList boardId={board._id} group={group} />
+                        <GroupFooter boardId={board._id} group={group} setBoard={setBoard} />
+                    </div>
+                
     </li>
 }
