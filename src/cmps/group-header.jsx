0f6b053833aa760037f25e20f4c6ca1784fb82e4
Lidor York <yorklidor@gmail.com>
2023-01-21 23:43:06 +0200

    dnd

--- src/cmps/group-header.jsx
+++ src/cmps/group-header.jsx
@@ -1,22 +1,41 @@
 import { saveBoard } from "../store/board.actions.js"
+import { useForm } from "../customHooks/useForm";
 
 export function GroupHeader({ group, board }) {
+    const groupId = group.id
+    const [groupTitleToSet, setGroupTitleToSet, handleChange] = useForm({ title: group.title })
 
     async function onRemoveGroup(groupId) {
         try {
-            const boardToEdit = { ...board }
-            const groups = boardToEdit.groups.filter((group) => group.id !== groupId)
-            boardToEdit.groups = groups
-            await saveBoard(boardToEdit)
+            const groups = board.groups.filter((group) => group.id !== groupId)
+            board.groups = groups
+            await saveBoard({ ...board })
         } catch (err) {
             console.error('Cannot remove group', err)
         }
     }
 
+    async function onSaveTitle() {
+        group.title = groupTitleToSet.title
+        console.log('groupt:', groupTitleToSet)
+        board.groups.forEach(group => { (group.id === groupId) && (group.title = groupTitleToSet.title) })
+        try {
+            console.log('board:', board)
+            await saveBoard({ ...board })
+        } catch (err) {
+            console.error('Can\'t save board!', err)
+        }
+    }
+
     return <section className="group-header">
         <section
+            onInput={console.log}
+            onBlur={onSaveTitle}
+            className="group-title focused"
             contentEditable={true}
             suppressContentEditableWarning={true}
+            value={groupTitleToSet.title}
+            name="title"
         >
             <section>{group.title}</section>
         </section>
--- src/customHooks/useForm.js
+++ src/customHooks/useForm.js
@@ -6,6 +6,7 @@ export const useForm = (initialState) => {
     function handleChange({ target }) {
         let { value, name: field, type, checked } = target
         // value = (type === 'number') ? +value : value
+        console.log('value:', value, field, type)
         switch (type) {
             case 'number':
             case 'range':
@@ -13,6 +14,7 @@ export const useForm = (initialState) => {
                 break;
             case 'checkbox':
                 value = checked
+                break;
             default:
                 break;
         }
