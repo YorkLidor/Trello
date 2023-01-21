a2769ad2310410065847c01b031244fa925bf320
matanad <matan19949@gmail.com>
2023-01-21 22:38:11 +0200

    ...

--- src/cmps/task-preview.jsx
+++ src/cmps/task-preview.jsx
@@ -24,33 +24,33 @@ export function TaskPreview({ task, group, boardId, isDragging }) {
     const labelsStyle = isLabelsLarge ? 'labels-large' : ''
 
     return <div className={`task-preview-container ${isDragging && 'is-dragging'}`}>
-            {/* COVER COLOR */}
-            {coverColor &&
-                <header className="cover-color" style={{ background: coverColor }}></header>
-            }
+        {/* COVER COLOR */}
+        {coverColor &&
+            <header className="cover-color" style={{ background: coverColor }}></header>
+        }
         <li className={`task-preview`} >
 
 
             {/* LABELS */}
-            {labels.length &&
-                <div className="labels-container" >
-                    {
-                        labels.map(label =>
-                            <div className="label-container" key={label.id}>
-                                <div
-                                    style={{ backgroundColor: isLabelsLarge ? label.color + '60' : label.color }}
-                                    onClick={toggleLabelsSize}
-                                    className={`labels-preview ${labelsStyle}`} >
-                                    {isLabelsLarge && <>
-                                        <div className="lable-circle" style={{ backgroundColor: label.color }}></div>
-                                        <span className='label-title' >{label.title}</span>
-                                    </>
-                                    }
-                                </div>
+            {(task.labels && task.labels.length) && <div className="labels-container" >
+                {console.log('labels:', labels)}
+                {
+                    labels.map(label =>
+                        <div className="label-container" key={label.id}>
+                            <div
+                                style={{ backgroundColor: isLabelsLarge ? label.color + '60' : label.color }}
+                                onClick={toggleLabelsSize}
+                                className={`labels-preview ${labelsStyle}`} >
+                                {isLabelsLarge && <>
+                                    <div className="lable-circle" style={{ backgroundColor: label.color }}></div>
+                                    <span className='label-title' >{label.title}</span>
+                                </>
+                                }
                             </div>
-                        )
-                    }
-                </div>
+                        </div>
+                    )
+                }
+            </div>
             }
 
 
