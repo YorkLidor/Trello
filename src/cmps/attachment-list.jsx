ffed4ff3da61dfe69c1f939dde2fca5a107dfde1
GalZohar4021 <118897719+GalZohar4021@users.noreply.github.com>
2023-01-21 20:01:41 +0200

    new modal - attachment viewer opens when clicking attachment on task details screen. modal contains delete and open in new tab options

--- src/cmps/attachment-list.jsx
+++ src/cmps/attachment-list.jsx
@@ -1,11 +1,11 @@
 import { AttachmentPreview } from './attachment-preview';
 
-export function AttachmentList({ task , onRemoveAttachment, onEditAttachment }) {
-
+export function AttachmentList({ task , attachmentProps }) {
+    
     return task?.attachments?.length > 0 && <ul className="attachment-list grid">
         {task.attachments.map(attachment =>
             <li className="attachment-preview" key={attachment.id}>
-                <AttachmentPreview attachment={attachment} onRemoveAttachment={onRemoveAttachment} onEditAttachment={onEditAttachment} />
+                <AttachmentPreview attachment={attachment} attachmentProps={attachmentProps} />
             </li>)}
     </ul>
 }
--- src/cmps/attachment-preview.jsx
+++ src/cmps/attachment-preview.jsx
@@ -1,17 +1,18 @@
 import { BsArrowUpRight } from 'react-icons/bs'
 
-export function AttachmentPreview({ attachment, onRemoveAttachment, onEditAttachment }) {
+export function AttachmentPreview({ attachment, attachmentProps }) {
+    const { onRemoveAttachment, onEditAttachment, onOpenAttachment } = attachmentProps
 
     return attachment && <div className="attachment-preview">
-        <img className="attachment-preview-img" src={attachment.url} alt={attachment.filename} />
-        <p className="attachment-details">
+        <img className="attachment-preview-img" src={attachment.url} alt={attachment.filename} onClick={() => onOpenAttachment(attachment)} />
+        <p className="attachment-details" onClick={(ev) => onOpenAttachment(ev, attachment)}>
             <span className="attachment-filename">{attachment.filename}</span>
             <BsArrowUpRight className='attachment-open' />
 
             <span className='attachment-info'>
                 <span className="attachment-date">{new Date(attachment.createdAt).toLocaleDateString()}</span>
                 <span className='attachment-info-break' />
-                <span className="action-attachment" onClick={() => onRemoveAttachment(attachment.id)}>Delete</span>
+                <span className="action-attachment" onClick={(ev) => onRemoveAttachment(ev, attachment.id)}>Delete</span>
                 <span className='attachment-info-break' />
                 <span className="action-attachment" onClick={(ev) => onEditAttachment(ev, attachment)}>Edit</span>
             </span>
