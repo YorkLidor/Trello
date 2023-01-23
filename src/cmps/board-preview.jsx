ac9eb3b689eb4a453863338689dcc4793a9b95cd
matanad <matan19949@gmail.com>
2023-01-23 14:36:18 +0200

    added star with effect

--- src/cmps/board-preview.jsx
+++ src/cmps/board-preview.jsx
@@ -1,13 +1,22 @@
+import { FaRegStar } from 'react-icons/fa'
+
 export function BoardPreview({ board, onBoardClick }) {
 
     return <li
         className={`board-preview ${!board && 'mod-add'}`}
-        onClick={(ev) => onBoardClick(ev,board?._id)}
+        onClick={(ev) => onBoardClick(ev, board?._id)}
         style={board && board.style}
     >
-        <span className="board-preview-fade">
+        <span className={`board-preview-fade ${!board ? 'add' : ''} flex column justify-between`}>
             {board && <h4 className="board-title">{board.title}</h4>}
             {!board && <p><span>Create new board</span></p>}
+
+            {board && <span className='details-container flex'>
+                <span className='star-container flex'>
+                    <FaRegStar />
+                </span>
+            </span>
+            }
         </span>
     </li>
 }
