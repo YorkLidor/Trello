5b2745d5a0a8c35669cf5afea94754844fa705fe
Lidor York <yorklidor@gmail.com>
2023-01-24 15:20:15 +0200

    homepage done before layout

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,19 +1,32 @@
 import { Link, NavLink } from "react-router-dom";
-import { SiTrello } from 'react-icons/si'
 
 export function AppHeader() {
 
-    return <header className="app-header">
-        <nav className="main-nav flex">
+    return <header className="app-header main-layout">
+
+        <div className="nav-container">
+
             <div className="logo-container">
                 <Link to="/" className="logo">
-                    <SiTrello/>
-                        Shmello
+                    <img src="https://frello-app.onrender.com/dist/Frello.8d010d74.svg" alt="" />
                 </Link>
             </div>
-            <nav className="navlinks-container">
-                <NavLink to="/workspace">Workspaces</NavLink>
-            </nav>
-        </nav >
+
+            <nav className="navlinks-container flex">
+                <NavLink className='workspace-link' to="/workspace">Workspaces</NavLink>
+            </nav >
+
+        </div>
+
+        <div className="login-container flex">
+            <a className="login" href="#">
+                Log in
+            </a>
+            <a className="get-shmello" href="#">
+                Get Shmello for free
+            </a>
+        </div>
+
+
     </header >
 }
