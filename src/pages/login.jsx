ca7e60818cf1ede5533ac424bd690e6d24d949c5
matanad <matan19949@gmail.com>
2023-01-26 01:43:31 +0200

    .

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 import { useForm } from "../customHooks/useForm"
+import { userService } from "../services/user.service"
+import { signup } from "../store/actions/user.actions"
 
 export function Login() {
-    const [user, setUser, handleChange] = useForm({ email: '', password: '' })
+    const [user, setUser, handleChange] = useForm(userService.getEmptyCredentials())
     const [isFilled, setIsFilled] = useState(false)
+    const navigate = useNavigate()
 
-    async function onLogIn() {
-        if (user.email && user.password) return
+    async function onLogIn(ev) {
+        ev.preventDefault()
+        console.log('user:', user)
+        if (!user.username || !user.password) return
+        try {
+            await signup(user)
+            navigate('/')
+        } catch (err) {
+            console.error(err.massage)
+        }
     }
 
     function onContinue(ev) {
         ev.preventDefault()
-        if (user.email) setIsFilled(true)
+        if (user.username) setIsFilled(true)
     }
 
     return <main className="login-page">
@@ -27,7 +39,7 @@ export function Login() {
             <input
                 className="input-email"
                 type="email"
-                name="email"
+                name="username"
                 placeholder="Enter email"
                 onChange={handleChange}
             />
@@ -40,17 +52,17 @@ export function Login() {
             />
             <button
                 className="btn-continue"
-                onClick={onContinue}
                 type="button"
+                onClick={onContinue}
             >
                 Continue
             </button>
-            <button className="btn-Login">Log in</button>
+            <button type="submit" className="btn-login">Log in</button>
             <section className="signup-link">
                 <a >Sign up for an account</a>
             </section>
         </form>
         <hr />
-        <img src="" alt="atlassian" />
+        <img className="atlassian-img" src="https://d2k1ftgv7pobq7.cloudfront.net/meta/c/p/res/images/16006ae28f149063408d601e8c80eddc/atlassian-logo-blue-small.svg" alt="atlassian" />
     </main>
 }
