92a3e6359534fa1c6edde4f60070236fa0ac2f5b
GalZohar4021 <118897719+GalZohar4021@users.noreply.github.com>
2023-01-26 01:03:18 +0200

    Cover TaskDetails: Styling the modal undesgined tools: Light picker buttons, remove cover and upload attach buttons

--- src/cmps/task-details/attachment/attachment-list.jsx
+++ src/cmps/task-details/attachment/attachment-list.jsx
@@ -1,5 +1,8 @@
-import { AttachmentPreview } from "./attachment-preview"
+import { boardService } from '../../../services/board.service'
 
+import { saveTask } from '../../../store/actions/board.actions'
+
+import { AttachmentPreview } from "./attachment-preview"
 import { onRemoveAttachment } from '../../../store/actions/board.actions'
 
 export function AttachmentList({ task, toggleModal, user, boardId, groupId }) {
@@ -9,10 +12,15 @@ export function AttachmentList({ task, toggleModal, user, boardId, groupId }) {
         onRemoveAttachment(user, boardId, groupId, task, attachment)
     }
 
+    async function onTaskUpdateCover(attachment) {
+        task.cover = { style: boardService.getCoverAttachStyle(attachment.url), fullSize: task.cover?.fullSize ? task.cover.fullSize : false }
+        await saveTask(boardId, groupId, task, boardService.getActivity(user, task, `${user.fullname} changed task ${task.title} cover to attachment ${attachment.filename}`))
+    }
+
     return task?.attachments?.length > 0 && <ul className="attachment-list">
         {task.attachments.map(attachment =>
             <li className="attachment-preview" key={attachment.id}>
-                <AttachmentPreview attachment={attachment} toggleModal={toggleModal} removeAttachment={removeAttachment} />
+                <AttachmentPreview attachment={attachment} toggleModal={toggleModal} removeAttachment={removeAttachment} onTaskUpdateCover={onTaskUpdateCover}/>
             </li>)}
     </ul>
 }
