381546dd5c2f8c22b6539652f010b4e35b3c9d20
matanad <matan19949@gmail.com>
2023-01-25 21:46:28 +0200

    New: userService

---
+++ src/services/user.service.js
@@ -0,0 +1,45 @@
+import { storageService } from "./async-storage.service"
+
+const STORAGE_KEY = 'userDB'
+const STORAGE_KEY_LOGGEDIN = 'loggedinUser'
+
+export const userService = {
+    getById,
+    login,
+    signup,
+    logout,
+    getLoggedinUser
+}
+
+function getById(userId) {
+    return storageService.get(STORAGE_KEY, userId)
+}
+
+async function login({ username, password }) {
+    return storageService.query(STORAGE_KEY)
+        .then(users => {
+            const user = users.find(user => user.username === username)
+            if (user) return _setLoggedinUser(user)
+            else return Promise.reject('Invalid login')
+        })
+}
+
+function signup({ username, password, fullname }) {
+    const user = { username, password, fullname, score: 10000 }
+    return storageService.post(STORAGE_KEY, user)
+        .then(_setLoggedinUser)
+}
+
+function logout() {
+    return sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN)
+}
+
+function getLoggedinUser() {
+    return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN))
+}
+
+function _setLoggedinUser(user) {
+    const userToSave = { _id: user._id, fullname: user.fullname, score: user.score }
+    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(userToSave))
+    return userToSave
+}
