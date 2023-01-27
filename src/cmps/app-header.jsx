eaf10cb0d6d97a42efba881b30ca80ef75d4d6cf
matanad <matan19949@gmail.com>
2023-01-28 00:13:13 +0200

    .

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -91,12 +91,16 @@ export function AppHeader() {
             </nav>
         </nav >
 
-        {user && <img
-            alt={user.fullname}
-            src={user.imgUrl}
-            className='list-member'
-            onClick={(ev) => onToggleModal(ev, USER_QUICK_MENU)}
-        />}
+        {user &&
+            <button className="btn-user">
+                <img
+                    alt={user.fullname}
+                    src={user.imgUrl}
+                    className='list-member'
+                    onClick={(ev) => onToggleModal(ev, USER_QUICK_MENU)}
+                />
+            </button>
+        }
         <div ref={elModal} className='modal-container'>
             {
                 modal?.isOpen && <Modal
