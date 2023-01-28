958bd1b8e160ce1aed898a0a755d7d0f9365991e
GalZohar4021 <118897719+GalZohar4021@users.noreply.github.com>
2023-01-28 14:25:44 +0200

    Delete & edit comment buttons show for user who did post the comment

--- frontend/src/cmps/task-details/comment/comment-preview.jsx
+++ frontend/src/cmps/task-details/comment/comment-preview.jsx
@@ -1,5 +1,10 @@
+import { useSelector } from "react-redux"
 
 export function CommentPreview({ comment }) {
+    const user = useSelector((storeState) => storeState.userModule.user)
+
+    console.log(user, comment.byMember)
+
     return <div className="comment-container flex row">
         <img className="commentor-logo" src={comment.byMember.imgUrl ? comment.byMember.imgUrl : 'https://res.cloudinary.com/dk2geeubr/image/upload/v1673890694/profileDefault_khqx4r.png'} />
         <div className="comment-box" key={comment.id}>
@@ -8,6 +13,14 @@ export function CommentPreview({ comment }) {
             <div className="task-activity">
                 <div className="comment-text">{comment.txt}</div>
             </div>
+            {
+                (user._id === comment.byMember.id) &&
+                <div className="comment-tools">
+                    <span className="edit-comment">Edit</span>
+                    <span className="break-tools"> • </span>
+                    <span className="delete-comment">Delete</span>
+                </div>
+            }
         </div>
-    </div >
+    </div>
 }
