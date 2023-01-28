5e123aae32cc3212c654b4c00b7cb15d17108cfb
GalZohar4021 <118897719+GalZohar4021@users.noreply.github.com>
2023-01-28 17:34:26 +0200

    added check if user is member is board before enable join action to card

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -4,13 +4,7 @@ import { httpService } from "./http.service"
 import { jUser } from "./jsons/board"
 import { utilService } from "./util.service"
 
-<<<<<<< HEAD
-const STORAGE_KEY = 'userDB'
 const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
-_createUsers()
-=======
-const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
->>>>>>> 8f4802919445605b72d930c87a7613a08bf252c6
 
 export const userService = {
     login,
@@ -36,30 +30,13 @@ async function login(userCred) {
     }
 }
 
-<<<<<<< HEAD
-async function login(userCred) {
-    try {
-        console.log('user', userCred);
-        const user = await httpService.post('auth/login', userCred)
-        if (user) {
-            return saveLocalUser(user)
-        }
-    } catch (err) {
-        console.log('Error with login', err)
-=======
 async function getById(userId) {
     try {
         return httpService.get(`${ROUTE}/${userId}`)
     } catch (err) {
         console.log('Couldnt get user', err)
->>>>>>> 8f4802919445605b72d930c87a7613a08bf252c6
         throw err
     }
-}
-
-function saveLocalUser(user) {
-    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
-    return user
 }
 
 function saveLocalUser(user) {
@@ -78,10 +55,6 @@ async function signup({ username, password, fullname, imgUrl = "http://some-img.
     }
 }
 
-<<<<<<< HEAD
-function logout() {
-    return sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
-=======
 async function logout() {
     try {
         sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
@@ -90,20 +63,10 @@ async function logout() {
         console.log('Error with login', err)
         throw err
     }
->>>>>>> 8f4802919445605b72d930c87a7613a08bf252c6
 }
 
 function getLoggedinUser() {
     return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
-<<<<<<< HEAD
-}
-
-function _setLoggedinUser(user) {
-    const userToSave = { _id: user._id, fullname: user.fullname, email: user.email, imgUrl: user.imgUrl }
-    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(userToSave))
-    return userToSave
-=======
->>>>>>> 8f4802919445605b72d930c87a7613a08bf252c6
 }
 
 function getEmptyCredentials() {
