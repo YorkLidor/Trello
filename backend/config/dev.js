b59a8a4b46b1b566304e972d51c8b303e984b04c
matanad <matan19949@gmail.com>
2023-01-28 16:52:31 +0200

    mongo db uploaded to atlas

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb://127.0.0.1:27017',
-  // dbURL: 'mongodb+srv://tommy:nboII9uOTnOfG6p8@cluster0.34orh.mongodb.net/?retryWrites=true&w=majority',
+  // dbURL: 'mongodb://127.0.0.1:27017',
+  dbURL: 'mongodb+srv://matan:YTbbaudPXFCLukHv@cluster0.hmtcbyg.mongodb.net/?retryWrites=true&w=majority',
 }
--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -4,24 +4,18 @@ import { httpService } from "./http.service"
 import { jUser } from "./jsons/board"
 import { utilService } from "./util.service"
 
-const STORAGE_KEY = 'userDB'
 const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
-_createUsers()
 
 export const userService = {
-    getById,
     login,
-    signup,
     logout,
+    signup,
+    update,
     getLoggedinUser,
+    getById,
     getEmptyCredentials
 }
-
-window.us = userService
-
-function getById(userId) {
-    return storageService.get(STORAGE_KEY, userId)
-}
+const ROUTE = 'user'
 
 async function login(userCred) {
     try {
@@ -36,33 +30,44 @@ async function login(userCred) {
     }
 }
 
+async function getById(userId) {
+    try {
+        return httpService.get(`${ROUTE}/${userId}`)
+    } catch (err) {
+        console.log('Couldnt get user', err)
+        throw err
+    }
+
+}
+
 function saveLocalUser(user) {
     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
     return user
 }
 
 async function signup({ username, password, fullname, imgUrl = "http://some-img.jpg", mentions = [] }) {
-    const user = { username, password, fullname, imgUrl, mentions }
+    const userCred = { username, password, fullname, imgUrl, mentions }
     try {
-        const signedUser = await storageService.post(STORAGE_KEY, user)
-        _setLoggedinUser(signedUser)
+        const user = await httpService.post('auth/signup', userCred)
+        return saveLocalUser(user)
     } catch (err) {
-        return err
+        console.log('Error with login', err)
+        throw err
     }
 }
 
-function logout() {
-    return sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
+async function logout() {
+    try {
+        sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
+        return await httpService.post('auth/logout')
+    } catch (err) {
+        console.log('Error with login', err)
+        throw err
+    }
 }
 
 function getLoggedinUser() {
     return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
-}
-
-function _setLoggedinUser(user) {
-    const userToSave = { _id: user._id, fullname: user.fullname, email: user.email, imgUrl: user.imgUrl }
-    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(userToSave))
-    return userToSave
 }
 
 function getEmptyCredentials() {
@@ -75,19 +80,13 @@ function getEmptyCredentials() {
     }
 }
 
-function _createUsers() {
-    let users = utilService.loadFromStorage(STORAGE_KEY)
-    if (!users || !users.length) {
-        users = [{ ...jUser, _id: utilService.makeId() }]
-        users.push({
-            _id: utilService.makeId(),
-            fullname: 'Shauli',
-            username: 'shauli',
-            email: 'shauli@gmail.com',
-            password: 'pitaAlHagaz',
-            imgUrl: "http://res.cloudinary.com/dk2geeubr/image/upload/v1674746367/qvbgfpwpobofvl5f6ocr.jpg",
-            mentions: []
-        })
-        utilService.saveToStorage(STORAGE_KEY, users)
+async function update(user) {
+    try {
+        user = await httpService.put(`${ROUTE}/${user._id}`, user)
+        if (getLoggedinUser()._id === user._id) saveLocalUser(user)
+        return user;
+    } catch (err) {
+        console.log('Couldnt update users', err)
+        throw err
     }
 }
