974a11a9999d6ac6e4fac6c32d598834f2d4ecee
matanad <matan19949@gmail.com>
2023-01-29 02:54:04 +0200

    redux error resolved. now the site will run on any device! 🎉

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,11 @@
-import { combineReducers, legacy_createStore as createStore } from "redux"
+import { combineReducers, legacy_createStore as createStore, compose } from "redux"
 
 import { boardReducer } from './reducers/board.reducer'
 import { appReducer } from './reducers/app.reducer'
 import { userReducer } from "./reducers/user.reducer"
 
-const middleware = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : () => { }
+// const middleware = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : () => { } 
+const middleware = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 
 
 const rootReducer = combineReducers({
