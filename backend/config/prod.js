d66badead230c3500fbf90581ae3ff9acdd6fd10
matanad <matan19949@gmail.com>
2023-02-21 20:39:58 +0200

    .

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://matan:adnLmKw20xkDFrSf@cluster0.hmtcbyg.mongodb.net/?retryWrites=true&w=majority',
 }
