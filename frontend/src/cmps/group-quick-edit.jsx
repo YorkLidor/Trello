b64fd855f557e832e8954325386b97ff652fb17d
matanad <matan19949@gmail.com>
2023-02-28 13:43:02 +0200

    .

--- frontend/src/cmps/group-quick-edit.jsx
+++ frontend/src/cmps/group-quick-edit.jsx
@@ -1,39 +1,59 @@
 import { useSelector } from "react-redux"
 import { ModalHeader } from "./modal/modal-header"
 import { closeModal } from "../../src/store/actions/app.actions"
+import { useRef, useState } from "react"
 
 export function GroupQuickEdit({ id, groupId, onRemoveGroup, onCopyGroup }) {
     const board = useSelector(state => state.boardModule.board)
     const modals = useSelector((storeState) => storeState.appModule.app.modals)
+    const modalHeader = useRef('List actions')
+    const [modalType, setModalType] = useState('')
+    const allowBack = useRef(false)
+
+    function onModalClickBack() {
+        modalHeader.current = 'List actions'
+        allowBack.current = false
+        setModalType('')
+    }
 
     function onItemClick(action) {
         switch (action) {
             case 'COPY':
-                onCopyGroup(board, groupId)
-                break;
+                // onCopyGroup(board, groupId)
+                modalHeader.current = 'Copy list'
+                allowBack.current = true
+                setModalType(action)
+                break
             case 'REMOVE':
                 onRemoveGroup(groupId)
-                break;
+                closeModal(modals, id)
+                break
             default:
-                break;
+                break
         }
-        closeModal(modals, id)
     }
 
     return <div className="modal-members-box quick-edit-modal-container">
 
-        <ModalHeader id={id} header={'List actions'} allowBack={false} />
+        <ModalHeader
+            id={id}
+            header={modalHeader.current}
+            allowBack={allowBack.current}
+            onModalClickBack={onModalClickBack}
+        />
 
-        <ul className="members-picker-list buttons-container">
-            <li>
-                Add card...
-            </li>
-            <li onClick={() => { onItemClick('COPY') }}>
-                Copy list...
-            </li>
-            <li onClick={() => { onItemClick('REMOVE') }}>
-                Delete list...
-            </li>
-        </ul>
+        {
+            !modalType && <ul className="members-picker-list buttons-container">
+                <li>
+                    Add card...
+                </li>
+                <li onClick={() => onItemClick('COPY')}>
+                    Copy list...
+                </li>
+                <li onClick={() => onItemClick('REMOVE')}>
+                    Delete list...
+                </li>
+            </ul>
+        }
     </div >
 }
